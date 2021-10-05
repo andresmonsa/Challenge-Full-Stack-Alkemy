b56a5377d6648826d28e4b371d4867689380c324
andresmonsa <monsalbefotografia@gmail.com>
2021-10-05 12:54:56 -0300

    fixing routes

--- Client/src/components/Routes/Routes.js
+++ Client/src/components/Routes/Routes.js
@@ -1,15 +1,17 @@
 import { useSelector } from 'react-redux'
-import { Route, Switch } from 'react-router'
+import { Route, Switch, Router } from 'react-router'
 import Home from '../Home/Home'
 import Login from '../Login/Login'
 
 export const RouterWeb = () => {
   const logged = useSelector(state => state.logged)
   return (
-    <Switch>
-      {logged
-        ? <Route exact path='/' component={Home} />
-        : <Route component={Login} />}
-    </Switch>
+    <Router basename='/Challenge-Full-Stack-Alkemy'>
+      <Switch>
+        {logged
+          ? <Route exact path='/' component={Home} />
+          : <Route component={Login} />}
+      </Switch>
+    </Router>
   )
 }
